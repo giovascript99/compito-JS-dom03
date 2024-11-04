301cf0dfdcb0d20488e21f932153db096c09aba0
Giovanni Perniola <gioperniola1999@gmail.com>
2024-11-04 16:31:06 +0100

    Selfwork dom 3 terminato e corretto

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
-let inputSecondi = document.querySelector("#inputSecondi");
-let startBtn = document.querySelector("#startBtn");
-let pauseBtn = document.querySelector("#pauseBtn");
-let clearBtn = document.querySelector("#clearBtn");
-let secondi = parseInt(inputSecondi.value);
+let remainingTime = document.querySelector('#remainingTime'); // tempo rimanente (countdown che visualizzeremo)
+let countdown_input = document.querySelector('#countdown_input'); // elemento che permette all'utente di inserire i secondi
+let startBtn = document.querySelector('#startBtn'); // button start
+let stopBtn = document.querySelector('#stopBtn'); // button stop 
+let resetBtn = document.querySelector('#resetBtn'); // button reset
+
+let interval;
+let counter;
+let remainingSeconds = 0;
 
 
 startBtn.addEventListener('click', () => {
-    if (isNaN(secondi) || secondi <= 0) {
-        secondiInput.value = "Valore non valido";
-        return;
+    clearInterval(interval)
+    counter = countdown_input.value; // secondi che inserirà l'utente nell'input
+
+    if(remainingSeconds !== 0){
+        counter = remainingSeconds;
     }
-    
-    let countdown = secondi;
-    secondiInput.value = countdown;
-    
-    const interval = setInterval(() => {
-        countdown--;
-        secondiInput.value = countdown;
-        if (countdown === 0) {
-            clearInterval(interval);
-            secondiInput.value = "Countdown Terminato!";
+
+    interval = setInterval(() => {
+        if(counter < 0){
+            clearInterval(interval)
+            remainingTime.innerHTML = 'tempo scaduto';
+
+        }else{
+            remainingTime.innerHTML = counter;
+            counter --;
         }
     }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(interval)
+    remainingSeconds = counter
+});
+
+resetBtn.addEventListener('click', () => {
+    countdown_input.value = '';
+    clearInterval(interval)
+    remainingTime.innerHTML = '--';
 })
